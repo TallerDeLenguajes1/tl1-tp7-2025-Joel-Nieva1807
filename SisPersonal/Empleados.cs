74c122d91a27e30f6d649c255df61605edfcb9d5
Joel Esteban Nieva Ovejero <nieva.joelesteban@gmail.com>
2025-06-05 12:45:50 -0300

    commit3

--- SisPersonal/Empleados.cs
+++ SisPersonal/Empleados.cs
@@ -1,5 +1,4 @@
 namespace EspacioEmpleados;
-
 public enum Cargos
 {
     Auxialiar,
@@ -9,30 +8,87 @@ public enum Cargos
     Investigador
 }
 
+
 public class Empleado
 {
+    
     private string nombre;
     private string apellido;
     public string Nombre
     {
 
         get { return nombre; }
-        set{ nombre = value; }
+        set { nombre = value; }
 
     }
-    
+
     public string Apellido
     {
         get { return apellido; }
         set { apellido = value; }
     }
 
-    public DateTime FechNacimiento { get; set;}
-    public char EstadoCivil { get; set;}
-    public DateTime FechaIngreso { get; set;}
-    public double SueldoBasico { get; set;}
+    public DateTime FechNacimiento { get; set; }
+    public char EstadoCivil { get; set; }
+    public DateTime FechaIngreso { get; set; }
+    public double SueldoBasico { get; set; }
 
-    public Cargos Cargo { get; set;}
+    public Cargos Cargo { get; set; }
+
+    public int Antiguedad(DateTime ingreso)
+    {
+        DateTime Hoy = DateTime.Now; // tomo la fecha de hoy
+
+        int antigued = Hoy.Year - ingreso.Year;
+
+        return antigued;
 
 
+    }
+
+    public int Edad(DateTime nacimiento)
+    {
+        int Anios = DateTime.Now.Year - nacimiento.Year;
+        return Anios;
+
+    }
+
+    public int Jubilacion(DateTime ingreso)
+    {
+        int jubi = 65 - (DateTime.Now.Year - ingreso.Year);
+
+        if (jubi <= 0) //por si ya se jubilo
+        {
+            jubi = 0;
+        }
+        return jubi;
+    }
+
+    public double Salario(int antigued, Cargos cargo, char EstadoCivil, double SueldoBasico)
+    {
+        double adicional=0;
+
+        if (antigued > 20)
+        {
+            adicional = adicional + (SueldoBasico * 0.25);
+
+        }
+        else
+        {
+            adicional = adicional + (SueldoBasico * (antigued / 100.0));
+            double i = adicional;
+             double ii = antigued;
+        }
+
+        if (cargo == Cargos.Ingeniero || cargo == Cargos.Especialista)
+        {
+            adicional = adicional * 1.50;
+        }
+        
+        if (EstadoCivil == 'c')
+        {
+            adicional += 150000;
+        }
+        return SueldoBasico+adicional;
+    }
 }
--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using EspacioEmpleados;
+
+class Program
+{
+
+    static void Main()
+    {
+        Empleado Trabajador = new Empleado();
+
+        string nombre = "Juan";
+        string apellido = "Perez";
+        DateTime ingreso = new DateTime(2010, 04, 8);
+        DateTime nacimiento = new DateTime(2001, 08, 20);
+        Trabajador.Cargo = Cargos.Ingeniero;
+        char EstadoCivil = 'c';
+        double SueldoBasico = 650000;
+
+        int antigued = Trabajador.Antiguedad(ingreso);
+        int edad = Trabajador.Edad(nacimiento);
+        int FaltaParaJubilarse = Trabajador.Jubilacion(ingreso);
+        double Salario = Trabajador.Salario(antigued, Trabajador.Cargo, EstadoCivil, SueldoBasico);
+
+
+
+
+
+        Console.WriteLine($"Los datos son: antiguedad tiene {antigued} anios, su edad {edad} anios y le faltan para jubilarse {FaltaParaJubilarse} anios");
+        Console.WriteLine($"El sueldo total es: ${Salario}");
+
+
+    }
+}
